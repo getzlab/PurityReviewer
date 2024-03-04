1d680ff1512c7ff7ebf604aea6e5f6fb5c0bc7ae
Claudia Chu <cchu@broadinstitute.org>
2024-03-04 12:45:10 -0500

    update reviewer example notebook

--- PurityReviewer/AppComponents/AbsoluteSolutionsReportComponent.py
+++ PurityReviewer/AppComponents/AbsoluteSolutionsReportComponent.py
@@ -237,7 +237,6 @@ def gen_absolute_solutions_report_layout():
     """
     return html.Div(
         children=[
-            html.H2('Absolute Solutions Table'),
             dash_table.DataTable(
                id='absolute-rdata-select-table',
                columns=[
