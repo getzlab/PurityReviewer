e134234030637fb1a4cb10efd71fb94a47c84ab0
njharlen <njharlen@broadinstitute.org>
2024-08-28 13:31:56 -0400

    Revert "commit"
    This reverts commit 6acab80b0cc6ee2f4cb9600b99e0e8c553caed25, reversing
    changes made to c1fb6157b9510b2b5f165f771e2046a0b276f295.

--- PurityReviewer/AppComponents/AbsoluteSolutionsReportComponent.py
+++ PurityReviewer/AppComponents/AbsoluteSolutionsReportComponent.py
@@ -246,6 +246,7 @@ def gen_absolute_solutions_report_layout():
     """
     return html.Div(
         children=[
+            html.H2('Absolute Solutions Table'),
             dash_table.DataTable(
                id='absolute-rdata-select-table',
                columns=[
